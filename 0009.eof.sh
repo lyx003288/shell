7d5a97b6cecc32ca24c68845cd66e1cc2a2f794d
yaxin.liu <yaxin.liu@ptmind.com>
2019-04-20 17:44:10 +0800

    EOF 用法

---
+++ 0009.eof.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ########################################################################################
+# EOF 用法
+# ########################################################################################
+
+function help()
+{
+cat << EOF
+    this is EOF usage
+    Anything you want to say at here, But don't break the law.
+That's important.
+                                                author: airy
+EOF
+}
+
+help
