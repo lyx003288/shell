50e4e61eaacebd0c8c595e14f9948400eb39ffc5
yaxin.liu <yaxin.liu@ptmind.com>
2019-04-23 12:00:51 +0800

    增加了一种随机数生成的方法

--- 0006.generate-random-num.sh
+++ 0006.generate-random-num.sh
@@ -7,13 +7,16 @@
 len=10
 
 # 方法一
+echo $RANDOM % 100 | bc
+
+# 方法二
 code=`tr -dc "A-Za-z0-9-\+=\!@#\$%\^\&*" < /dev/urandom | head -c ${len}`
 echo "m1: generate random number is ${code}, the length is ${len}"
 
-# 方法二
+# 方法三
 code=`date +%s%N | md5sum | head -c ${len}`
 echo "m2: generate random number is ${code}, the length is ${len}"
 
-# 方法三
+# 方法四
 code=`cat /proc/sys/kernel/random/uuid | md5sum | cut -f1 -d" " | head -c ${len}`
 echo "m3: generate random number is ${code}, the length is ${len}"
